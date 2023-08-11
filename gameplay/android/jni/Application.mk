be61c85359c5ef4e7a4cc4ab6656d1003f0d465d
aaw <tmiura@atmarkartworks.com>
2023-08-11 16:21:36 +0900

    setting samples browser

---
+++ gameplay/android/jni/Application.mk
@@ -0,0 +1,7 @@
+#NDK_TOOLCHAIN_VERSION := 4.8
+APP_CPPFLAGS += -std=c++11
+APP_STL      := c++_static
+APP_MODULES  := libgameplay
+APP_ABI      := arm64-v8a armeabi-v7a x86 x86_64
+APP_PLATFORM := android-19
+
---
+++ samples/browser/Application.mk
@@ -0,0 +1,7 @@
+#NDK_PROJECT_PATH := /home/aaw/src/GP/gameplay/app-browser/browser/android/jni
+#NDK_TOOLCHAIN_VERSION := 4.8
+APP_CPPFLAGS += -std=c++11
+#APP_STL      := gnustl_static
+APP_STL      := c++_shared
+APP_ABI      := armeabi-v7a x86 arm64_v8a x86_64
+APP_PLATFORM := android-19
